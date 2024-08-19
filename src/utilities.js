76f325bc6268974218db351bdf705b57263f5248
Ivan Pimenov <strauc@mail.ru>
2024-08-20 02:50:06 +0400

    fix

--- src/utilities.js
+++ src/utilities.js
@@ -4,11 +4,11 @@ const calculator = (expression) => {
   const [num1, sign, num2] = expression.split(' ');
   if (sign === '+') {
     return String(+num1 + +num2);
-  } else if (sign === '-') {
-    return String(+num1 - +num2);
-  } else {
-    return String(+num1 * +num2);
   }
+  if (sign === '-') {
+    return String(+num1 - +num2);
+  }
+  return String(+num1 * +num2);
 };
 
 export { randomNumber, calculator };
